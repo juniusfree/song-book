bc31f864f349331ebd56ddd1078fd602c58534ce
Junius Free B. Fontamillas <8164667+juniusfree@users.noreply.github.com>
2023-06-12 16:13:51 +0800

    refactor: hide from mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="md:hidden p-4">
+          <p className="text-gray-700">
+            Sorry! This application is not supported on mobile devices. Please
+            use a desktop browser.
+          </p>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+        </div>
+
+        <div className="hidden md:block">{children}</div>
+      </body>
     </html>
-  )
+  );
 }
